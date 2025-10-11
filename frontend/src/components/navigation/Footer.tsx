0e3a3d89abed6534ce8c00a056d7db8935eccfe1
kursat <cakmakkur@hotmail.com>
2025-10-11 17:43:52 +0200

    update: homepage

--- frontend/src/components/navigation/Footer.tsx
+++ frontend/src/components/navigation/Footer.tsx
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="position-absolute bottom-0 start-0 d-flex w-100 justify-content-end bg-dark p-3">
-      <Link to="/aboutus" className="footer-link">
-        About us
-      </Link>
+    <footer className="footer d-flex w-100 justify-content-end align-items-center bg-dark p-3">
+      <Link to="/aboutus">About us</Link>
     </footer>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,21 @@
-const Home = () => {
-  return <div>Home Page</div>;
-};
-export default Home;
+import { Link } from "react-router-dom";
+import ShopItem from "../components/product/ShopItem";
+
+export default function HomePage() {
+  return (
+    <div className="homepage-main d-flex flex-column align-items-center text-center gap-4 p-4">
+      <h1>Willkommen!</h1>
+      <h2>Entdecken Sie hochwertige Instrumente</h2>
+
+      <Link to="/listing" className="btn btn-primary">
+        Jetzt entdecken
+      </Link>
+
+      <div className="d-flex flex-wrap justify-content-center gap-4 mt-4">
+        <ShopItem />
+        <ShopItem />
+        <ShopItem />
+      </div>
+    </div>
+  );
+}
