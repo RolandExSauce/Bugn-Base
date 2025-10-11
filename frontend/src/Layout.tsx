b62476570fc5c2127295acff90d5a1033dd5d81b
kursat <cakmakkur@hotmail.com>
2025-10-11 17:06:11 +0200

    update: about us page

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/navigation/NavBar";
+import Footer from "./components/navigation/Footer";
 
 export default function Layout() {
   return (
@@ -8,6 +9,7 @@ export default function Layout() {
       <main className="main-div">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
